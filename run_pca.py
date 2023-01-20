3e7776098d75dee14b2169973fcda6cc537dbaa5
alappe <alexander.lappe@uni-tuebingen.de>
2023-01-20 16:22:19 +0100

    Added some comments to the run file.

--- run_pca.py
+++ run_pca.py
@@ -1,30 +1,32 @@
 from SparsePCA import *
 import pandas as pd
+
+# Load data (change path to location of data file)
 Y = pd.read_excel(r"C:\Users\Alex\Documents\Uni\VojtaPCA\Data\keyPointsAllVideosED.xlsx")
+# Convert to numpy
 X = Y.to_numpy(copy=True)
-vars = np.expand_dims((np.var(X, axis=1)), 1)
-print(vars)
 
 # Normalize columns
-# X = np.random.randn(1770, 21) * np.sqrt(vars)
 X -= np.mean(X, axis=0)
-print(np.min(X))
-# print(np.mean(X, axis=0))
-# print(np.var(X, axis=1))
-# print(X.shape)
+# Might make sense to also standardize the features but I left them as they were for now
 
+# Number of components to compute
 n_components = 5
-lambd = 10000
+# Set hyperparameter. Larger value means stronger sparsification. Need to play around with this value.
+lambdas = 10000
+# Set learning rate, number of iterations and alpha value to control the approximation of the absolute value.
+# Can probably be left unchanged
 lr = 1e-6
 steps = 10000
 alpha = 100
 
+# Instantiate the SparsePCA class.
 sparse_pca = SparsePCA(n_components=n_components, alpha=alpha, verbose=True)
 
-W = sparse_pca.fit(X, lambdas=10000)
+# Fit the sparse PCA. W will be a matrix that contains the components.
+W = sparse_pca.fit(X, lambdas=lambdas)
+# Print some useful properties of the found components
 print("Explained variance:", sparse_pca.explained_variance)
 print("Total Explained variance", np.sum(sparse_pca.explained_variance))
 print("W.T @ W:", W.T @ W)
 print("L1 norm of components:", np.linalg.norm(W, ord=1, axis=0))
-
-# [3.66100924 2.61949157 3.10564342 2.57092353 3.66520359]
